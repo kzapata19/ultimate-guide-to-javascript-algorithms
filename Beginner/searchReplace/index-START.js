9493f14d8d2026c5278d0029cb03b0fde8190595
Karen Zapata <kzapata0919@gmail.com>
2021-05-28 13:10:18 -0700

    Add solution using regex and replace

--- Beginner/searchReplace/index-START.js
+++ Beginner/searchReplace/index-START.js
@@ -12,7 +12,16 @@ Note: Make sure to match the casing(capitalization) of the string to be replaced
 */
 
 // Solution using replace:
+// function searchReplace(str, word, newWord) {
+//   if(word[0] === word[0].toUpperCase()) newWord = newWord[0].toUpperCase() + newWord.slice(1)
+//   return str.replace(word, newWord)
+// }
+
+// Solution using RegEx:
 function searchReplace(str, word, newWord) {
-  if(word[0] === word[0].toUpperCase()) newWord = newWord[0].toUpperCase() + newWord.slice(1)
-  return str.replace(word, newWord)
+  let regex = new RegExp(word, "gi")
+  if(/[A-Z]/.test(word[0])) newWord = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+  return str.replace(regex, newWord)
 }
+
+console.log(searchReplace("hi there my name is Bellini", "Bellini", "Tuna"))
