1b3bc0dc38e30d021c172343b62551a21084eab6
Karen Zapata <kzapata0919@gmail.com>
2021-05-27 23:28:00 -0700

    Add optimized solution to iterate through the text until reaching mid-point to avoid redundant comparisons

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -12,14 +12,29 @@ e.g palindromeChecker('racecar') // will return true
 //     return reversed === text;
 // }
 
-// Looping through and comparing characters:
-function palindromeChecker(text) {
-    let lowercasedArr = text.toLowerCase().split("");
-    return lowercasedArr.every((letter, index) => {
-        return letter === lowercasedArr[text.length - index - 1]
-    })
+// Looping through and comparing characters: NOTE that this solution is redundant
+// because it continues to compare letters beyond the half-way mark of the text
+// function palindromeChecker(text) {
+//     let lowercasedArr = text.toLowerCase().split("");
+//     return lowercasedArr.every((letter, index) => {
+//         return letter === lowercasedArr[text.length - index - 1]
+//     })
+// }
+
+// OPTIMIZED solution: Using a for loop to eliminate redundancy in solution above
+// only loop through half the length of the text
+// an odd length palindrome will have matching letters and the middle letter will remain
+function palindromeChecker(text) {  
+    let textLength = text.length;
+    for(let i = 0; i < text.length/2; i++) {
+        if(text[i] !== text[textLength - i - 1]) return false
+    }
+    return true;
 }
 
-
-
 module.exports = palindromeChecker;
+
+// performance check:
+// The optimized solution is the fastest (only iterate through until mid-point)
+// The un-optimized iterative solution comes in second, about 47% slower
+// The solution using the built-in methods comes in last, about 87% slower
