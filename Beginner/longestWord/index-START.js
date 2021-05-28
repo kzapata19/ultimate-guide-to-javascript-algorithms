d4c37b0a5864cf75c098db279a75734ce6b5d7b6
Karen Zapata <kzapata0919@gmail.com>
2021-05-28 12:33:33 -0700

    Add solution using reduce

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -3,11 +3,50 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
+/*
+Parameters: one string
+
+Return: one string, the longest word in the input string
+
+Edge Cases/Examples:
+longestWord("hi, my name is Bellini") -> "Bellini"
+longestWord("what about numbers? This one is large: 1010101010") -> "1010101010"
+// if there is a tie, return the first word in the text of the longest length
+longestWord("what if there is a tie, karen ?") -> "there"
+
+Pseudocode:
+- Split the string into an array of strings using the space as a separator
+- Iterate through the text array
+    - Keep track of the current longest word's index
+    - Keep track of the current longest word's length to compare the next word
+    - Update both trackers if the current word is longer than the previous longest word
+- Return the longest word
+*/
 
 
+// For-loop solution:
+// function longestWord(text) {
+//     let wordsArray = text.split(" ");
+//     let longestWordInd = 0;
+//     let currentLongestWordLen = 0;
+//     for(let i = 0; i < wordsArray.length; i++) {
+//         let currentLength = wordsArray[i].length;
+//         if(currentLength > currentLongestWordLen) {
+//             longestWordInd = i;
+//             currentLongestWordLen = currentLength;
+//         }
+//     }
+//     return wordsArray[longestWordInd];
+// }
+
+// Using reduce:
 function longestWord(text) {
-    // Code goes here
-}
+    let wordsArray = text.split(" ");
+   return wordsArray.reduce((longestWord, currentWord) => {
+    if(currentWord.length > longestWord.length) longestWord = currentWord
+    return longestWord
+   }, "")
 
+}
 
 module.exports = longestWord
